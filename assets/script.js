6c87130a16bdf8e425c0d2efda89fc943401356c
daniash84 <danielle.hess84@gmail.com>
2023-07-06 15:41:56 -0400

    update-3

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,28 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength = 8;
-// Write password to the #password input
+
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
   var characters = ["!", "@", "#", "$", "%", "^", "&", "*"];
 }
 
 function generatePassword() {
-// Add event listener to generate button
+
 var user = window.prompt("Enter characters between 8-128.");
 var passwordLength = parseInt(user);
-if (passwordLength < 8 || passwordLength > 128) {
+if (isNan(passwordLength)) {
   window.alert("Please enter characters between 8-128 in length.");
-  return;
+  return;}
+  else
+  console.log("Thank you for entering a valid length");
 }
-}
-
-// Write password to the #password input
- var password = generatePassword();
-  var passwordText = document.querySelector("#password");
- 
- generateBtn.addEventListener("click", writePassword);
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-  passwordText.value = password;
-
 
 function writePassword() {
+ var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
+generateBtn.addEventListener("click", writePassword);
