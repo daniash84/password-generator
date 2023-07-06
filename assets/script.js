d80d1f203dd27cebf616a9670bc758e04b685c85
daniash84 <danielle.hess84@gmail.com>
2023-07-06 15:16:42 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordLength = 8;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var numbers = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9";
+  var letters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t","u","v","w","x","y","z";
+  passwordText.value = password;
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
+// Write password to the #password input
+ var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+ 
+
+function writePassword() {
+}
